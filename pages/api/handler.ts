a423dd488e8c8b66da0bcdba913b5e779c0c2af8
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2023-05-19 16:55:17 +0200

    feat: mock api

--- pages/api/handler.ts
+++ pages/api/handler.ts
@@ -1,13 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ 
+    '0xE6cf1795036e8C88fdAdBE035282b0A3856e8D82': 1 
+  })
 }
